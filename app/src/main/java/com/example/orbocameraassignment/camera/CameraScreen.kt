6ef7b5cacd6e4813d60099ca0261910812e73aed
vinay-singh-dev <vv4011273@gmail.com>
2026-08-11 08:32:06 +0530

    Added Camera Preview

--- app/src/main/java/com/example/orbocameraassignment/camera/CameraScreen.kt
+++ app/src/main/java/com/example/orbocameraassignment/camera/CameraScreen.kt
@@ -2,6 +2,7 @@ package com.example.orbocameraassignment.camera
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.view.ViewGroup
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
@@ -16,13 +17,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import com.otaliastudios.cameraview.CameraView
+import com.otaliastudios.cameraview.controls.Audio
 
 @Composable
 fun CameraScreen() {
 
     val context = LocalContext.current
-
+    val lifecycleOwner = LocalLifecycleOwner.current
     var hasCameraPermission by remember {
         mutableStateOf(
             ContextCompat.checkSelfPermission(
@@ -49,7 +54,19 @@ fun CameraScreen() {
         contentAlignment = Alignment.Center
     ) {
         if (hasCameraPermission) {
-            Text("Camera permission granted")
+            AndroidView(
+                modifier = Modifier.fillMaxSize(),
+                factory = { context ->
+                    CameraView(context).apply {
+                        layoutParams = ViewGroup.LayoutParams(
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            ViewGroup.LayoutParams.MATCH_PARENT
+                        )
+                        audio = Audio.OFF
+                        setLifecycleOwner(lifecycleOwner)
+                    }
+                }
+            )
         } else {
             Text("Camera permission is required")
         }
